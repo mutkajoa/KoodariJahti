ae03369a1ea316e16b5fe83b0c0703c8656e172a
mutkajoa <33824586+mutkajoa@users.noreply.github.com>
2020-02-21 16:12:31 +0200

    installed node

---
+++ server.js
@@ -0,0 +1,8 @@
+const FRAME_RATE = 1000/60
+
+//dependencies
+const express = require('express')
+const http = require('http')
+const morgan = require('morgan')
+const path = require('path')
+const socketIO = require('socket.io')
